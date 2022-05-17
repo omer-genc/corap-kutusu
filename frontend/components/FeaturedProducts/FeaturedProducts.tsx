5596011ac9ab559ac03dd3db693d5a984cad8392
Ömer <omergnc1@gmail.com>
2022-05-17 12:20:55 +0300

    Finish | featured product

--- frontend/components/FeaturedProducts/FeaturedProducts.tsx
+++ frontend/components/FeaturedProducts/FeaturedProducts.tsx
@@ -1,46 +1,105 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Product from '../Product';
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
 const FeaturedProducts = () => {
+  const [slidesPerView, setSlidesPerView] = React.useState(1);
+
+  useEffect(() => {
+    const calculateSlidesPerView = () => {
+      if (innerWidth < 768) return 1;
+      if (innerWidth < 976) return 2;
+      if (innerWidth < 1280) return 3;
+
+      return 4;
+    };
+
+    return () => {
+      setSlidesPerView(calculateSlidesPerView());
+    };
+  }, []);
+
   return (
     <div className="container mx-auto mt-28">
       <h2 className="text-4xl text-center font-bold text-navy-blue">
-        FeaturedProducts
+        Featured Products
       </h2>
-      <div className="flex justify-around gap-4 flex-wrap mt-16">
-        <Product
-          variant="featured"
-          product={{
-            name: 'Product a 1',
-            price: 100,
-            image: 'https://picsum.photos/id/134/320/320',
-          }}
-        />
-        <Product
-          variant="featured"
-          product={{
-            name: 'Product 1',
-            price: 100,
-            image: 'https://picsum.photos/id/114/800/320',
-          }}
-        />
-        <Product
-          variant="featured"
-          product={{
-            name: 'Product 1',
-            price: 100,
-            image: 'https://picsum.photos/id/32/320/320',
-          }}
-        />
-        <Product
-          variant="featured"
-          product={{
-            name: 'Product 1',
-            price: 100,
-            image: 'https://picsum.photos/id/33/320/320',
-          }}
-        />
-      </div>
+      <Swiper
+        slidesPerView={slidesPerView}
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mt-8"
+      >
+        <SwiperSlide>
+          <Product
+            variant="featured"
+            product={{
+              name: 'Product a 1',
+              price: 100,
+              image: 'https://picsum.photos/id/104/320/320',
+            }}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Product
+            variant="featured"
+            product={{
+              name: 'Product a 1',
+              price: 100,
+              image: 'https://picsum.photos/id/114/320/320',
+            }}
+          />
+        </SwiperSlide>
+        <SwiperSlide className="flex justify-center">
+          <Product
+            variant="featured"
+            product={{
+              name: 'Product a 1',
+              price: 100,
+              image: 'https://picsum.photos/id/124/320/320',
+            }}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Product
+            variant="featured"
+            product={{
+              name: 'Product a 1',
+              price: 100,
+              image: 'https://picsum.photos/id/134/320/320',
+            }}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Product
+            variant="featured"
+            product={{
+              name: 'Product a 1',
+              price: 100,
+              image: 'https://picsum.photos/id/144/320/320',
+            }}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Product
+            variant="featured"
+            product={{
+              name: 'Product a 1',
+              price: 100,
+              image: 'https://picsum.photos/id/154/320/320',
+            }}
+          />
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 };
